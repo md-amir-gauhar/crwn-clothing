a18a5acb5cf9a6cf5438116652d74f4da605ac15
md-amir-gauhar <gauharamir15@gmail.com>
2021-06-28 12:38:42 +0530

    created respectiove selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collections-overview/CollectionOverview.js
+++ src/components/collections-overview/CollectionOverview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectShopCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selector';
 import CollectionPreview from '../../components/preview-collection/CollectionPreview';
 
 import './CollectionOverview.scss';
@@ -20,7 +20,7 @@ const CollectionOverview = ({ collections }) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
+  collections: selectCollectionForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverview);
